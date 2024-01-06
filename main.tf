14dd9b882cf0a633bd5b323b9474aa01f713373d
SRTPI <149040756+SRTPI@users.noreply.github.com>
2024-01-06 14:46:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ provider "aws" {
 access_key = var.AWS_ACCESS_KEY
 secret_key = var.AWS_SECRET_KEY
 region = "ap-south-1"
-} resource "aws_instance" "myec2" {
+}
+resource "aws_instance" "myec2" {
   ami           = "ami-08fe36427228eddc4"
   instance_type = "t2.micro"
   vpc_security_group_ids=[aws_security_group.web-sg.id]
